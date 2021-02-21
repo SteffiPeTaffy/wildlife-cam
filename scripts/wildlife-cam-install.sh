3c39483473fd453ca0b668fa48411bb2e9c5ea8e
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-02-21 16:17:38 +0100

    remove faulty quotes when writing ini file

--- scripts/wildlife-cam-install.sh
+++ scripts/wildlife-cam-install.sh
@@ -67,7 +67,7 @@ touch ${CONFIG_FILE_NAME}
 # create folder for photos
 mkdir ${PHOTO_DIR_NAME}
 echo -e "[General]" >> ${CONFIG_FILE_NAME}
-echo -e "PhotoDirPath=\"${HOME_DIR}${BASE_DIR_NAME}${PHOTO_DIR_NAME}\"" >> ${CONFIG_FILE_NAME}
+echo -e "PhotoDirPath=${HOME_DIR}${BASE_DIR_NAME}${PHOTO_DIR_NAME}" >> ${CONFIG_FILE_NAME}
 echo -e "" >> ${CONFIG_FILE_NAME}
 
 # Configure Telegram
@@ -77,12 +77,9 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
     read -rp "Type Telegram API Token (like 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11): " TELEGRAM_API_TOKEN
     read -rp "Type Telegram Chat ID (like 0123456789): " TELEGRAM_CHAT_ID
     echo ""
-    echo "Your Telegram details:"
-    echo "Api Token : $TELEGRAM_API_TOKEN"
-    echo "Chat Id   : $TELEGRAM_CHAT_ID"
     echo -e "[Telegram]" >> ${CONFIG_FILE_NAME}
-    echo -e "ApiKey=\"$TELEGRAM_API_TOKEN\"" >> ${CONFIG_FILE_NAME}
-    echo -e "ChatId=\"$TELEGRAM_CHAT_ID\"" >> ${CONFIG_FILE_NAME}
+    echo -e "ApiKey=$TELEGRAM_API_TOKEN" >> ${CONFIG_FILE_NAME}
+    echo -e "ChatId=$TELEGRAM_CHAT_ID" >> ${CONFIG_FILE_NAME}
     echo -e "" >> ${CONFIG_FILE_NAME}
 fi
 
