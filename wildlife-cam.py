5936b8ef4733505408c96ac4ffffde990f5201da
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-02-21 12:28:54 +0100

    wildlife cam - basic motion detection

--- wildlife-cam.py
+++ wildlife-cam.py
@@ -1,1 +1,31 @@
-print('Hello Wildife Cam')
+import RPi.GPIO as GPIO
+import time
+
+# GPIO pin for the PIR sensor
+PIR_GPIO_PIN = 14
+
+# setup GPIO pin for the PIR (motion) sensor
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIR_GPIO_PIN, GPIO.IN)
+
+# time to wait after taking a photo in seconds
+WAIT_TIME = 2
+
+
+#  take a photo when motion is detected
+def motion_detected(pir_sensor):
+    print("wildlife-cam: Motion detected.")
+
+
+# do the loop
+while True:
+    try:
+        print("wildlife-cam: Waiting for motion.")
+        GPIO.wait_for_edge(PIR_GPIO_PIN, GPIO.RISING)
+        motion_detected(PIR_GPIO_PIN)
+        print("wildlife-cam: Sleeping for " + str(WAIT_TIME) + " seconds.")
+        time.sleep(WAIT_TIME)
+    except:
+        print("wildlife-cam: Stopping due to keyboard interrupt.")
+        GPIO.cleanup()
+        break
