c89b0acf5816e9637cddaf4ba12d5399c0dbd59f
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-02-22 14:56:41 +0100

    WIP - streaming camera image using flask

--- wildlife-cam-web/app.py
+++ wildlife-cam-web/app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+#!/usr/bin/env python
+from flask import Flask, render_template, Response
+from camera import Camera
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
