a308aa1437aa28e46aa68d67df2628a9b76bfcf8
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-02-22 13:28:11 +0100

    WIP - remove faulty flags

--- scripts/wildlife-cam-install.sh
+++ scripts/wildlife-cam-install.sh
@@ -50,11 +50,11 @@ sudo apt-get --yes install libqtgui4
 sudo apt-get --yes install libqt4-test
 sudo apt-get --yes install libhdf5-dev
 
-sudo pip3 --yes install flask
-sudo pip3 --yes install numpy
-sudo pip3 --yes install opencv-contrib-python
-sudo pip3 --yes install imutils
-sudo pip3 --yes install opencv-python
+sudo pip3 install flask
+sudo pip3 install numpy
+sudo pip3 install opencv-contrib-python
+sudo pip3 install imutils
+sudo pip3 install opencv-python
 
 
 # Check if Wildlife Cam folder already exists
