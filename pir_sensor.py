ee3943799ed5c8aa964abb938156c6585f0216fc
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-02-25 18:16:58 +0100

    fix pir sensor

--- pir_sensor.py
+++ pir_sensor.py
@@ -2,13 +2,13 @@ import RPi.GPIO as GPIO
 
 
 class Sensor(GPIO):
-    def __init__(self, config):
+    def __init__(self, config, *args, **kwargs):
         self.pin = int(config['Pin'])
-        super().__init__()
+        super().__init__(*args, **kwargs)
 
     def run(self):
-        self.setmode(GPIO.BCM)
-        self.setup(self.pin, GPIO.IN)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.IN)
 
     def add_motion_detected_handler(self, handle_motion_detected):
-        self.add_event_detect(self.pin, GPIO.RISING, callback=lambda pir: handle_motion_detected())
+        GPIO.add_event_detect(self.pin, GPIO.RISING, callback=lambda pir: handle_motion_detected())
