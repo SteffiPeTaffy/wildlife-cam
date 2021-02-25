fb3bd98e778d1363a7f2f51a4b667abe4ba58f2e
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-02-25 23:17:12 +0100

    simplify ftp uploader

--- ftp_uploader.py
+++ ftp_uploader.py
@@ -18,22 +18,18 @@ class Uploader:
         cnopts.hostkeys = None
         with pysftp.Connection(host=self.sftp_host, port=self.sftp_port, username=self.sftp_username,
                                password=self.sftp_password, cnopts=cnopts) as srv:
+
             base_path, file_name = os.path.split(file_path)
             _, sub_folder_name = os.path.split(base_path)
 
-            with srv.cd(self.sftp_dir):
+            try:
+                srv.cd(self.sftp_dir)
                 if not srv.exists(sub_folder_name):
-                    with srv.mkdir(sub_folder_name):
-                        with srv.cd(sub_folder_name):
-                            try:
-                                srv.put(file_path)
-                            except Exception as e:
-                                logger.info("wildlife-cam: Failed to uploaded file")
-                                logger.exception(e)
-                            try:
-                                srv.get(file_name)
-                                logger.info("wildlife-cam: Successfully uploaded file")
-                            except FileNotFoundError:
-                                logger.error("wildlife-cam: Failed to uploaded file")
+                    srv.mkdir(sub_folder_name)
+                srv.cd(sub_folder_name)
+                srv.put(file_path, file_name)
+            except Exception as e:
+                logger.info("wildlife-cam: Failed to uploaded file")
+                logger.exception(e)
 
         srv.close()
