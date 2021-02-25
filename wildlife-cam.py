dc1e272267075b701a1eef6f71c700756fb47a2b
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-02-25 21:33:42 +0100

    fix while motion condition

--- wildlife-cam.py
+++ wildlife-cam.py
@@ -16,7 +16,7 @@ def motion_detected(pir):
     logger.info("wildlife-cam: Motion detected")
     camera.snap_photo()
     count = 0
-    while pir.motion_detected() and count < 4:
+    while pir.motion_detected and count < 4:
         time.sleep(0.2)
         camera.snap_photo()
         count += 1
@@ -60,19 +60,19 @@ if config.has_section('SFTP'):
 
 # Setup PIR sensor
 pir_sensor_pin = int(config['PirSensor']['Pin'])
-pir = MotionSensor(pir_sensor_pin)
+pir_sensor = MotionSensor(pir_sensor_pin)
 
 # GPIO.setmode(GPIO.BCM)
 # GPIO.setup(pir_sensor_pin, GPIO.IN)
 # GPIO.add_event_detect(pir_sensor_pin, GPIO.RISING, motion_detected)
 
 try:
-    pir.wait_for_no_motion(2)
+    pir_sensor.wait_for_no_motion(2)
     logger.info("wildlife-cam: Ready and waiting for motion")
     while True:
-        pir.wait_for_motion()
-        motion_detected(pir)
-        pir.wait_for_no_motion(10)
+        pir_sensor.wait_for_motion()
+        motion_detected(pir_sensor)
+        pir_sensor.wait_for_no_motion(10)
 finally:
     logger.info("wildlife-cam: Stopping Wildlife Cam")
     camera.close()
