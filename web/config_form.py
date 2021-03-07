c52d02b0af257842f478fb7dca50b56a8178bcea
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-03-07 12:31:38 +0100

    add telegram to web config

--- web/config_form.py
+++ web/config_form.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, IntegerField
+from wtforms import StringField, SubmitField, PasswordField, IntegerField, BooleanField
 from wtforms.validators import DataRequired
+
+from custome_validators import RequiredIf
 
 
 class ConfigForm(FlaskForm):
     gpio_pin = IntegerField("PIR Sensor Pin: ", validators=[DataRequired()])
+    telegram_enabled = BooleanField("Telegram")
+    telegram_api_key = PasswordField("Telegram API Key: ", validators=[RequiredIf('telegram_enabled')])
+    telegram_chat_id = StringField("Telegram Chat ID: ", validators=[RequiredIf('telegram_enabled')])
+    ftp_enabled = BooleanField("FTP Upload")
+    ftp_ip_address = StringField("FTP IP Address: ", validators=[RequiredIf('ftp_enabled')])
+    ftp_ip_port = IntegerField("FTP Port: ", validators=[RequiredIf('ftp_enabled')])
+    ftp_ip_user = StringField("FTP User: ", validators=[RequiredIf('ftp_enabled')])
+    ftp_ip_password = PasswordField("FTP Password: ", validators=[RequiredIf('ftp_enabled')])
+    ftp_ip_directory = StringField("FTP Directory: ", validators=[RequiredIf('ftp_enabled')])
     submit = SubmitField("Submit")
--- web/web.py
+++ web/web.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import configparser
+import os
 from flask import Flask, render_template, request
-
 from config_form import ConfigForm
 
 app = Flask(__name__)
@@ -18,13 +18,27 @@ def load_config():
 def config_form():
     form = ConfigForm()
     if form.validate_on_submit():
-        gpio_pin = form.gpio_pin.data
-        print(gpio_pin)
-
         config = load_config()
+
+        gpio_pin = form.gpio_pin.data
         config.set('PirSensor', 'Pin', str(gpio_pin))
+
+        telegram_enabled = form.telegram_enabled.data
+        telegram_api_key = form.telegram_api_key.data
+        telegram_chat_id = form.telegram_chat_id.data
+        if telegram_enabled:
+            if not config.has_section('Telegram'):
+                config.add_section('Telegram')
+            config.set('Telegram', 'ApiKey', telegram_api_key)
+            config.set('Telegram', 'ChatId', telegram_chat_id)
+        else:
+            if config.has_section('Telegram'):
+                config.remove_section('Telegram')
+
         with open('/home/pi/WildlifeCam/WildlifeCam.ini', 'w') as configfile:
             config.write(configfile)
+
+        os.system("sudo systemctl restart wildlife-cam")
 
     return render_template('index.html', form=form)
 
@@ -35,6 +49,10 @@ def index():
 
     form = ConfigForm()
     form.gpio_pin.data = config.getint('PirSensor', 'Pin')
+
+    form.telegram_enabled.data = config.has_section('Telegram')
+    form.telegram_api_key.data = config.get('Telegram', 'ApiKey')
+    form.telegram_chat_id.data = config.get('Telegram', 'ChatId')
 
     return render_template('index.html', form=form)
 
