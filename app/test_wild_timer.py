d2ec5f13e8f1d84f3ac9a49b4de6ed767448bea7
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-03-23 10:44:53 +0100

    Extracted fixture setup into a proper setUp method [Steffi/Lukasz]

--- app/test_wild_timer.py
+++ app/test_wild_timer.py
@@ -4,25 +4,23 @@ import time
 
 
 class TestResettableTimer(TestCase):
-    ended = False
+
+    def setUp(self):
+        self.ended = False
+        self.timer = ResettableTimer(interval=1, timeout=3,
+                            function=self.has_triggered)
 
     def test_start(self):
-        self.ended = False
-        timer = ResettableTimer(interval=1, timeout=30,
-                                function=self.has_triggered)
-        timer.start()
+        self.timer.start()
         self.assertFalse(self.ended)
         time.sleep(1.1)
         self.assertTrue(self.ended)
 
     def test_reset(self):
-        self.ended = False
-        timer = ResettableTimer(interval=1, timeout=30,
-                                function=self.has_triggered)
-        timer.start()
+        self.timer.start()
         self.assertFalse(self.ended)
         time.sleep(0.8)
-        timer.reset()
+        self.timer.reset()
         self.assertFalse(self.ended)
         time.sleep(0.5)
         self.assertFalse(self.ended)
@@ -30,29 +28,22 @@ class TestResettableTimer(TestCase):
         self.assertTrue(self.ended)
 
     def test_timeout(self):
-        self.ended = False
-        timer = ResettableTimer(interval=1, timeout=5,
-                                function=self.has_triggered)
-        timer.start()
+        self.timer.start()
         time.sleep(0.8)
-        timer.reset()
+        self.timer.reset()
         time.sleep(0.8)
-        timer.reset()
+        self.timer.reset()
         time.sleep(3.5)
         self.assertTrue(self.ended)
 
     def test_not_reset_after_timeout_reached(self):
-        self.ended = False
-        timer = ResettableTimer(interval=1, timeout=3,
-                                function=self.has_triggered)
-
-        timer.start()
+        self.timer.start()
         time.sleep(0.8)
-        timer.reset()
+        self.timer.reset()
         time.sleep(0.8)
-        timer.reset()
+        self.timer.reset()
         time.sleep(0.8)
-        timer.reset()
+        self.timer.reset()
         time.sleep(0.8)
         self.assertTrue(self.ended)
 
