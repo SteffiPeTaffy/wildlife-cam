224543ea6e80cb2e874d8ed100bd5076d93f8b17
Stefanie Grewenig <sgreweni@thoughtworks.com>
2021-03-23 10:36:42 +0100

    add test to check timer cannot be resetted after timeout reached

--- app/test_wild_timer.py
+++ app/test_wild_timer.py
@@ -2,40 +2,58 @@ from unittest import TestCase
 from wild_timer import ResettableTimer
 import time
 
+
 class TestResettableTimer(TestCase):
     ended = False
 
     def test_start(self):
         self.ended = False
-        timer = ResettableTimer(interval=5, timeout=30,
+        timer = ResettableTimer(interval=1, timeout=30,
                                 function=self.has_triggered)
         timer.start()
-        time.sleep(6)
+        self.assertFalse(self.ended)
+        time.sleep(1.1)
         self.assertTrue(self.ended)
 
     def test_reset(self):
         self.ended = False
-        timer = ResettableTimer(interval=5, timeout=30,
+        timer = ResettableTimer(interval=1, timeout=30,
                                 function=self.has_triggered)
         timer.start()
-        time.sleep(4)
+        self.assertFalse(self.ended)
+        time.sleep(0.8)
         timer.reset()
         self.assertFalse(self.ended)
-        time.sleep(4)
+        time.sleep(0.5)
         self.assertFalse(self.ended)
-        time.sleep(2)
+        time.sleep(0.6)
         self.assertTrue(self.ended)
 
     def test_timeout(self):
         self.ended = False
-        timer = ResettableTimer(interval=5, timeout=10,
+        timer = ResettableTimer(interval=1, timeout=5,
                                 function=self.has_triggered)
         timer.start()
-        time.sleep(4)
+        time.sleep(0.8)
         timer.reset()
-        time.sleep(4)
+        time.sleep(0.8)
         timer.reset()
-        time.sleep(2.01)
+        time.sleep(3.5)
+        self.assertTrue(self.ended)
+
+    def test_not_reset_after_timeout_reached(self):
+        self.ended = False
+        timer = ResettableTimer(interval=1, timeout=3,
+                                function=self.has_triggered)
+
+        timer.start()
+        time.sleep(0.8)
+        timer.reset()
+        time.sleep(0.8)
+        timer.reset()
+        time.sleep(0.8)
+        timer.reset()
+        time.sleep(0.8)
         self.assertTrue(self.ended)
 
     def has_triggered(self):
